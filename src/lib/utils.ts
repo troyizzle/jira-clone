5e48fe7b25d2943660b0d5e17c8307b8c1ff4698
Troy Underwood <troyizzle@gmail.com>
2024-10-15 16:29:35 -0400

    feat: add members

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,53 +1,18 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { createTRPCNext } from '@trpc/next';
-import { AppRouter } from "@/server";
-import { httpBatchLink } from "@trpc/client";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function saltAndHashPassword(password: string) {
-  return password
-}
+export function generateInviteCode(length: number) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
 
-function getBaseUrl() {
-  if (typeof window !== 'undefined')
-    // browser should use relative path
-    return '';
-  if (process.env.VERCEL_URL)
-    // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`;
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-  // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-}
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
 
-export const trpc = createTRPCNext<AppRouter>({
-  config(opts) {
-    return {
-      links: [
-        httpBatchLink({
-          /**
-           * If you want to use SSR, you need to use the server's full URL
-           * @see https://trpc.io/docs/v11/ssr
-           **/
-          url: `${getBaseUrl()}/api/trpc`,
-          // You can pass any HTTP headers you wish here
-          async headers() {
-            return {
-              // authorization: getAuthCookie(),
-            };
-          },
-        }),
-      ],
-    };
-  },
-  /**
-   * @see https://trpc.io/docs/v11/ssr
-   **/
-  ssr: false,
-});
+  return result;
+}
