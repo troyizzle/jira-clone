7e10df856720e5945e9afa8827c383c8e4f54902
Troy Underwood <troyizzle@gmail.com>
2024-10-15 16:19:43 -0400

    feat: add workspace switcher

--- src/app/features/workspaces/server/route.ts
+++ src/app/features/workspaces/server/route.ts
@@ -6,6 +6,21 @@ import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 import { ID } from "node-appwrite";
 
 const app = new Hono()
+  .get(
+    "/",
+    sessionMiddleware,
+    async (c) => {
+      const databases = c.get("databases");
+      const user = c.get("user");
+
+      const workspaces = await databases.listDocuments(
+        DATABASE_ID,
+        WORKSPACES_ID,
+      );
+
+      return c.json({ data: workspaces });
+    }
+  )
   .post(
     "/",
     zValidator("form", createWorkspaceSchema),
