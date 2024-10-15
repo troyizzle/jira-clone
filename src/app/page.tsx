0222dbb03f2d56290b7ee15da5f1b2919fe70701
Troy Underwood <troyizzle@gmail.com>
2024-10-14 22:47:47 -0400

    feat: add logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,30 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useCurrent } from "./features/auth/api/use-current";
+import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
+import { useLogout } from "./features/auth/api/use-logout";
 
 export default function Home() {
+  const router = useRouter();
+
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+
+  }, [data])
   return (
     <div className="">
-      <Button>
-        Primary
-      </Button>
+      Only visisble to authorized users
 
-      <Button variant="secondary">
-        Secondary
-      </Button>
-
-      <Button variant="destructive">
-        Destructive
-      </Button>
-
-      <Button variant="ghost">
-        Ghost
-      </Button>
-
-      <Button variant="muted">
-        Muted
-      </Button>
-
-      <Button variant="outline">
-        Outline
-      </Button>
-
-      <Button variant="tertiary">
-        tet
+      <Button onClick={() => mutate()}>
+        Logout
       </Button>
     </div>
   );
