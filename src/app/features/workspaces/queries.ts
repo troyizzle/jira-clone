5b2dccb56cd1b1ba818b3160f5c9b85aa5b22ad0
Troy Underwood <troyizzle@gmail.com>
2024-10-15 22:22:45 -0400

    chore: use createSessionClient in workspace queries

--- src/app/features/workspaces/queries.ts
+++ src/app/features/workspaces/queries.ts
@@ -4,21 +4,11 @@ import { AUTH_COOKIE } from "../auth/constants";
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
 import { getMember } from "../members/utils";
 import { Workspace } from "./types";
+import { createSessionClient } from "@/lib/appwrite";
 
 export const getWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
-
-    const session = cookies().get(AUTH_COOKIE);
-
-    if (!session) return { documents: [], total: 0 }
-
-    client.setSession(session.value);
-    const databases = new Databases(client);
-    const account = new Account(client);
-
+    const { databases, account } = await createSessionClient();
     const user = await account.get();
 
     const members = await databases.listDocuments(
@@ -53,17 +43,7 @@ interface GetWorkspaceProps {
 
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
-
-    const session = cookies().get(AUTH_COOKIE);
-
-    if (!session) return null;
-
-    client.setSession(session.value);
-    const databases = new Databases(client);
-    const account = new Account(client);
+    const { account, databases } = await createSessionClient();
 
     const user = await account.get();
 
