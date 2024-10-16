1c345dc200a2a4f61338f6c8457d974ec09e7888
Troy Underwood <troyizzle@gmail.com>
2024-10-15 22:20:28 -0400

    feat: refactor session client

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,6 +1,32 @@
 import "server-only"
 
-import { Account, Client } from "node-appwrite"
+import { Account, Client, Databases } from "node-appwrite"
+import { cookies } from "next/headers";
+import { AUTH_COOKIE } from "@/app/features/auth/constants";
+
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+
+  const session = cookies().get(AUTH_COOKIE);
+
+  if (!session || !session.value) {
+    throw new Error("Unauthorized");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client)
+    },
+
+    get databases() {
+      return new Databases(client);
+    }
+  }
+}
 
 export async function createAdminClient() {
   const client = new Client()
